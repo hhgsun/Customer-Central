e4a9d7df945f0e4690c44e2ddc37811cc8ecbc00
hhgsun <hhgsun@gmail.com>
2021-09-19 12:47:55 +0300

    env change

--- server/src/environment-example.php
+++ server/src/environment-example.php
@@ -1,5 +1,16 @@
 <?php
-$dbhost = 'db-host';
-$dbuser = 'db-user';
-$dbpass = 'db-password';
-$dbname = 'db-name';
+$isDev = TRUE; // yayınlanmadan önce burası yorum satırı hale getirilmelidir.
+
+// #### PRODUCTION
+$dbhost = '{db-host}';
+$dbuser = '{db-user}';
+$dbpass = '{db-password}';
+$dbname = '{db-name}';
+
+// #### DEVELOPMENT
+if(isset($isDev) && $isDev) {
+  $dbhost = '{dev-db-host}';
+  $dbuser = '{dev-db-user}';
+  $dbpass = '{dev-db-password}';
+  $dbname = '{dev-db-name}';
+}
